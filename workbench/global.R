d6ada44d9829bfe09576a83732425d0e038d131c
mmondelli <malumondelli@gmail.com>
2018-04-04 13:03:27 -0300

    minor fixes

--- workbench/global.R
+++ workbench/global.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(ggplot2)
-library(RPostgreSQL)
+#library(RPostgreSQL)
 library(sqldf)
 library(formattable)
 library(RColorBrewer)
@@ -22,13 +22,13 @@ colours <- c(hue_pal(h = c(0, 30000) + 360, c = 110, l = 65, h.start = 10,
 
 #ajuste <- par(mar=c(1,4,3,1)+0.6)
 
-drv <- dbDriver("PostgreSQL")
+#drv <- dbDriver("PostgreSQL")
 #con <- dbConnect(SQLite(), "~/Dropbox/Artigos/2015-OswaldoTrelles (1)/database/swift_provenance041.db")
 #con <- dbConnect(SQLite(), "~/Dropbox/loss/prov/run001op2.db")
-con <- dbConnect(SQLite(), "~/Dropbox/loss/swift_provenance.db")
+con <- dbConnect(SQLite(), "./swift_provenance.db")
 #con <- dbConnect(SQLite(), "~/Dropbox/LNCC/Dissertação/Resultados/DB/swift_provenance.db")
 #con <- dbConnect(SQLite(), "~/scriptsSwift/gecko_peerj.db")
 #con <- dbConnect(drv, dbname = "swift_provenance",
 #                 host = "localhost", port = 5432,user = "postgres", password = 'postgres')
 
-script_names <- as.vector(dbGetQuery(con, "select distinct script_filename from script_run")[,1])
+script_names <- as.vector(dbGetQuery(con, "select distinct script_filename from script_run where script_filename like '%loss_all%')[,1])
